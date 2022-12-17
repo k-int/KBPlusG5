b67b79640815f04d39cb42c122c44bc8cd3003a2
Ian Ibbotson <ianibbo@gmail.com>
2022-12-17 15:54:03 +0000

    Added vanilla funcitonal test to see how github copes

---
+++ kbplus/src/integration-test/groovy/kbplus/PublicHomeSpec.groovy
@@ -0,0 +1,28 @@
+package kbplus
+
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
+
+import geb.spock.*
+
+/**
+ * See http://www.gebish.org/manual/current/ for more instructions
+ */
+@Integration
+@Rollback
+class PublicHomeSpec extends GebSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        when:"The home page is visited"
+            go '/'
+
+        then:"The title is correct"
+        	title == "Welcome to Grails"
+    }
+}
